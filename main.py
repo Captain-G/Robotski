2cd9f2ae5475087e6d524b2bff05f11e36338306
Captian-G <poppinpites7@gmail.com>
2022-11-12 13:23:26 +0300

    Added clapping and arms fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # edit vector files using https://www.photopea.com/
 import pygame
+from pygame import mixer
 
 pygame.init()
 
@@ -62,6 +63,26 @@ right_hand_raised_img = pygame.image.load("right_hand_raised.png")
 right_hand_raised_x = 440
 right_hand_raised_y = 155
 
+left_arm_folded_img = pygame.image.load("left_arm_folded.png")
+left_arm_folded_x = 110
+left_arm_folded_y = 150
+
+right_arm_folded_img = pygame.image.load("right_arm_folded.png")
+right_arm_folded_x = 445
+right_arm_folded_y = 150
+
+clap_left_img = pygame.image.load("clap_left.png")
+clap_left_x = 300
+clap_left_y = 165
+
+clap_right_img = pygame.image.load("clap_right.png")
+clap_right_x = 390
+clap_right_y = 162
+
+clap_text_img = pygame.image.load("clap_text.png")
+clap_text_x = 350
+clap_text_y = 315
+
 head_left = False
 head_right = False
 head_forward = True
@@ -69,10 +90,14 @@ head_forward = True
 left_hand_down = True
 left_hand_half = False
 left_hand_raised = False
+left_hand_folded = False
 
 right_hand_down = True
 right_hand_half = False
 right_hand_raised = False
+right_hand_folded = False
+
+clapping = False
 
 
 def robotBody(x, y):
@@ -115,10 +140,27 @@ def rightHandRaised(x, y):
     screen.blit(right_hand_raised_img, (x, y))
 
 
+def leftArmFolded(x, y):
+    screen.blit(left_arm_folded_img, (x, y))
+
+
+def rightArmFolded(x, y):
+    screen.blit(right_arm_folded_img, (x, y))
+
+
+def clap(l_x, l_y, r_x, r_y, t_x, t_y):
+    screen.blit(clap_left_img, (l_x, l_y))
+    screen.blit(clap_right_img, (r_x, r_y))
+    screen.blit(clap_text_img, (t_x, t_y))
+    # clap_sound = mixer.Sound("clap.wav")
+    # clap_sound.play()
+
+
 state = "forward"
 running = True
 while running:
     screen.blit(background, (0, 0))
+    robotBody(robot_x, robot_y)
     # pygame.draw.line(screen, (0, 0, 0), (80, 0), (80, 650), width=2)
     # pygame.draw.line(screen, (0, 0, 0), (0, 50), (80, 50), width=2)
     # pygame.draw.line(screen, (0, 0, 0), (0, 110), (80, 110), width=2)
@@ -152,35 +194,62 @@ while running:
                 left_hand_down = True
                 left_hand_half = False
                 left_hand_raised = False
+                left_hand_folded = False
             elif event.key == pygame.K_KP4:
                 left_hand_down = False
                 left_hand_half = True
                 left_hand_raised = False
+                left_hand_folded = False
             elif event.key == pygame.K_KP7:
                 left_hand_down = False
                 left_hand_half = False
                 left_hand_raised = True
-
-            if event.key == pygame.K_KP3:
+                left_hand_folded = False
+            elif event.key == pygame.K_KP3:
                 right_hand_down = True
                 right_hand_half = False
                 right_hand_raised = False
+                right_hand_folded = False
             elif event.key == pygame.K_KP6:
                 right_hand_down = False
                 right_hand_half = True
                 right_hand_raised = False
+                right_hand_folded = False
             elif event.key == pygame.K_KP9:
                 right_hand_down = False
                 right_hand_half = False
                 right_hand_raised = True
-
-            if event.key == pygame.K_KP0:
+                right_hand_folded = False
+            elif event.key == pygame.K_KP0:
                 right_hand_down = True
                 right_hand_half = False
                 right_hand_raised = False
+                right_hand_folded = False
                 left_hand_down = True
                 left_hand_half = False
                 left_hand_raised = False
+                left_hand_folded = False
+                clapping = False
+            elif event.key == pygame.K_KP2:
+                right_hand_down = False
+                right_hand_half = False
+                right_hand_raised = False
+                right_hand_folded = False
+                left_hand_down = False
+                left_hand_half = False
+                left_hand_raised = False
+                left_hand_folded = False
+                clapping = True
+            elif event.key == pygame.K_KP8:
+                right_hand_down = False
+                right_hand_half = False
+                right_hand_raised = False
+                right_hand_folded = True
+                left_hand_down = False
+                left_hand_half = False
+                left_hand_raised = False
+                left_hand_folded = True
+                clapping = False
 
     if head_left:
         headLeft(left_x, left_y)
@@ -195,6 +264,10 @@ while running:
         leftArmHalf(left_arm_half_x, left_arm_half_y)
     elif left_hand_raised:
         leftHandRaised(left_hand_raised_x, left_hand_raised_y)
+    elif left_hand_folded:
+        leftArmFolded(left_arm_folded_x, left_arm_folded_y)
+    elif clapping:
+        clap(clap_left_x, clap_left_y, clap_right_x, clap_right_y, clap_text_x, clap_text_y)
 
     if right_hand_down:
         rightHandDown(right_hand_down_x, right_hand_down_y)
@@ -202,11 +275,9 @@ while running:
         rightArmHalf(right_arm_half_x, right_arm_half_y)
     elif right_hand_raised:
         rightHandRaised(right_hand_raised_x, right_hand_raised_y)
-
-    robotBody(robot_x, robot_y)
-
-    # screen.blit(left_arrow_img, (left_arrow_x, left_arrow_y))
-    # screen.blit(right_arrow_img, (right_arrow_x, right_arrow_y))
-    # screen.blit(front_arrow_img, (front_arrow_x, front_arrow_y))
+    elif right_hand_folded:
+        rightArmFolded(right_arm_folded_x, right_arm_folded_y)
+    elif clapping:
+        clap(clap_left_x, clap_left_y, clap_right_x, clap_right_y, clap_text_x, clap_text_y)
 
     pygame.display.update()
